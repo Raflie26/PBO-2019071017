2ccaea226b959f2860d67bac98e95baa1013659f
Raflie <71370737+Raflie26@users.noreply.github.com>
2020-10-12 21:53:03 +0700

    Add files via upload

---
+++ Class3.java
@@ -0,0 +1,12 @@
+package com.method;
+
+public class Class3 {
+    static int myMethod(int x){
+        return 5 + x;
+    }
+
+    public static void main(String[] args){
+        System.out.println(myMethod(3));
+
+    }
+}
